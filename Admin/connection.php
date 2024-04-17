961a3ead76fa184cd15ce2acfc965587f4a33d78
Abdullah <abdullahkhan221121@gmail.com>
2024-04-17 17:37:43 +0500

    done

--- Admin/connection.php
+++ Admin/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = new mysqli("localhost" , "root" , "" , "bookstoref22");
+    $conn = new mysqli("localhost" , "root" , "" , "bookstoref2");
 
     if(!$conn){
         die("Connection Failed");
